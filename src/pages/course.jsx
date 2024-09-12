e99b7bf45779884fdbffd58481008e2f73fa34f1
aouf83 <premierinteriors16@gmail.com>
2024-09-12 09:58:49 +0530

    created

--- src/pages/course.jsx
+++ src/pages/course.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../assets/styles/course.css';
+import { IoIosLock } from "react-icons/io";
 
 const Courses = () => {
     const courses = [
         { id: 1, title: 'UI Engineer', image: 'https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/UI_Engineer.jpg' },
-        { id: 2, title: 'Backend Developer', image: 'https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/UI_Engineer.jpg' },
-        { id: 3, title: 'DevOps Engineer', image: 'https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/UI_Engineer.jpg' },
-        { id: 4, title: 'Web Application Developer', image: 'https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/UI_Engineer.jpg' },
-        { id: 5, title: 'Mobile Application Developer', image: 'https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/UI_Engineer.jpg' },
+        { id: 2, title: 'Backend Developer', image: 'https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/Backend_Developer_ACsVD3G.jpg' },
+        { id: 3, title: 'DevOps Engineer', image: 'https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/DevOps_Engineer.jpg' },
+        { id: 4, title: 'Web Application Developer', image: 'https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/Web_Application_Developer_vmaZxha.jpg' },
+        { id: 5, title: 'Mobile Application Developer', image: 'https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/Mobile_Application_Developer.jpg' },
     ];
 
     return (
@@ -15,13 +16,14 @@ const Courses = () => {
             {courses.map((course, index) => (
                 <div key={course.id} className="course-card">
                     <div className="course-image-container">
+
                         <img src={course.image} alt={course.title} className="course-image" />
                     </div>
                     <div className="course-content">
                         <span className="course-number">#{index + 1}</span>
                         <h3>{course.title}</h3>
                     </div>
-                    <img src="https://d3mbaugvr53zg5.cloudfront.net/media/elearning/designation/UI_Engineer.jpg" alt="lock" className="lock-icon" />
+                    <IoIosLock className="lock-icon" />
                 </div>
             ))}
         </div>
